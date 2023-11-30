efcf9f5bf71985efb08b4b90e893c4395b6b2097
Timo <38291523+lovetodream@users.noreply.github.com>
2023-11-30 18:48:30 +0100

    chore: cleanup platform requirement

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 
 let package = Package(
     name: "swift-blake2",
-    platforms: [.macOS(.v12)], // remove after perf testing
     products: [
         .library(name: "Blake2", targets: ["Blake2"]),
     ],
@@ -12,7 +11,7 @@ let package = Package(
         .testTarget(
             name: "Blake2Tests",
             dependencies: ["Blake2"],
-            resources: [.copy("TestVectors/blake2-kat.json")]
+            resources: [.copy("TestVectors")]
         ),
     ]
 )
