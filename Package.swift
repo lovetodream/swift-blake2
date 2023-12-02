a6f53361e5b509466654400b9a1a36a655f337cc
Timo <38291523+lovetodream@users.noreply.github.com>
2023-12-02 12:25:29 +0100

    docs: add inline documentation

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@ import PackageDescription
 let package = Package(
     name: "swift-blake2",
     products: [
-        .library(name: "Blake2", targets: ["Blake2"]),
+        .library(name: "BLAKE2", targets: ["BLAKE2"]),
     ],
     targets: [
-        .target(name: "Blake2"),
+        .target(name: "BLAKE2"),
         .testTarget(
-            name: "Blake2Tests",
-            dependencies: ["Blake2"],
+            name: "BLAKE2Tests",
+            dependencies: ["BLAKE2"],
             resources: [.copy("TestVectors")]
         ),
     ]
--- Tests/BLAKE2Tests/BLAKE2bTests.swift
+++ Tests/BLAKE2Tests/BLAKE2bTests.swift
@@ -1,37 +1,37 @@
 import XCTest
-@testable import Blake2
+@testable import BLAKE2
 
-final class Blake2bTests: XCTestCase {
+final class BlAKE2bTests: XCTestCase {
     let tests = Resources.inst.blake2testCases().blake2b
 
-    func testSimpleApi() {
+    func testSimpleApi() throws {
         for test in self.tests {
-            let computed = Blake2b.hash(input: Array(test.in), key: Array(test.key))
-            XCTAssertEqual(Array(test.out), computed)
+            let computed = try BLAKE2b.hash(data: test.in, key: test.key)
+            XCTAssertEqual(test.out, computed)
         }
     }
 
-    func testStreamingApi() {
+    func testStreamingApi() throws {
         let tests = self.tests.keyed
         var buf = [UInt8](repeating: 0, count: tests.count)
         for i in 0..<buf.count {
             buf[i] = UInt8(i)
         }
-        for step in 1..<Blake2b.Constants.BLOCKBYTES {
+        for step in 1..<BLAKE2b.Constants.BLOCKBYTES {
             for i in 0..<tests.count {
                 let test = tests[i]
-                var blake2 = Blake2b(key: Array(test.key))
+                var blake2 = try BLAKE2b(key: test.key)
 
                 var mlen = i, start = 0
 
                 while mlen >= step {
-                    blake2.update(input: Array(buf[start..<start+step]))
+                    blake2.update(data: buf[start..<start+step])
                     start += step
                     mlen -= step
                 }
-                blake2.update(input: Array(buf[start..<start+mlen]))
+                blake2.update(data: buf[start..<start+mlen])
                 let hash = blake2.finalize()
-                XCTAssertEqual(Array(test.out), hash)
+                XCTAssertEqual(test.out, hash)
             }
         }
     }
