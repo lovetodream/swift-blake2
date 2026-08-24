7d0fc1118bfc55819a2abb7a223c6cdb473a64c4
Timo <38291523+lovetodream@users.noreply.github.com>
2026-08-24 19:46:50 +0200

    add benchmarks
    Signed-off-by: Timo <38291523+lovetodream@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.2
 import PackageDescription
 
 let package = Package(
@@ -15,3 +15,23 @@ let package = Package(
         ),
     ]
 )
+
+if Context.environment["ENABLE_BLAKE2_BENCHMARKS"] != nil {
+    package.platforms = [.macOS(.v26)]
+    package.dependencies.append(
+        .package(url: "https://github.com/ordo-one/benchmark", from: "1.0.0")
+    )
+    package.targets.append(
+        .executableTarget(
+            name: "BLAKE2Benchmarks",
+            dependencies: [
+                .product(name: "Benchmark", package: "benchmark"),
+                "BLAKE2",
+            ],
+            path: "Benchmarks/BLAKE2Benchmarks",
+            plugins: [
+                .plugin(name: "BenchmarkPlugin", package: "benchmark")
+            ]
+        )
+    )
+}
